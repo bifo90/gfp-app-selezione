19928313c9dda55a104946a6bd4f68b93375a739
bifo90 <stefanobifolco@gmail.com>
2025-11-02 08:58:06 +0100

    aggiunta nome e cognome utente

--- db/migrate/20251102075153_create_users.rb
+++ db/migrate/20251102075153_create_users.rb
@@ -3,6 +3,8 @@ class CreateUsers < ActiveRecord::Migration[8.1]
     create_table :users do |t|
       t.string :email_address, null: false
       t.string :password_digest, null: false
+      t.string :first_name
+      t.string :last_name
 
       t.timestamps
     end
